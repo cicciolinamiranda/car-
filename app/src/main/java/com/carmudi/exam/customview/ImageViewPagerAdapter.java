8d2f931457127f97f4209c1bd82fc299adf62c80
Cicciolina Miranda <cicciolina@cicciolina-macbookpro.roam.corp.google.com>
2018-05-19 08:00:07 +0800

    remove unused listener for ImageSwitcher viewa

--- app/src/main/java/com/carmudi/exam/customview/ImageViewPagerAdapter.java
+++ app/src/main/java/com/carmudi/exam/customview/ImageViewPagerAdapter.java
@@ -4,16 +4,12 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.support.annotation.ArrayRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Size;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.carmudi.exam.R;
 import com.carmudi.exam.customview.listener.OnItemClickListener;
-import com.carmudi.exam.customview.listener.OnSetImageListener;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -25,16 +21,13 @@ import java.util.ArrayList;
 
 public class ImageViewPagerAdapter extends ViewPagerAdapter {
 
-    protected static final String TAG = ImageViewPagerAdapter.class.getSimpleName();
     protected Context mContext;
     protected OnItemClickListener mOnPagerItemClick;
-    protected OnSetImageListener mOnSetImageListener;
 
     protected ArrayList<String> urls;
 
-    public ImageViewPagerAdapter(Context context,@NonNull OnSetImageListener onSetImageListener ) {
+    public ImageViewPagerAdapter(Context context) {
         mContext = context;
-        mOnSetImageListener = onSetImageListener;
         urls = new ArrayList<>();
     }
 
@@ -57,10 +50,6 @@ public class ImageViewPagerAdapter extends ViewPagerAdapter {
             }
         });
 
-
-        if(mOnSetImageListener != null ){
-            mOnSetImageListener.setImage(imageView,position);
-        }
         return imageView;
     }
 
@@ -69,29 +58,11 @@ public class ImageViewPagerAdapter extends ViewPagerAdapter {
         return urls.size();
     }
 
-    public void setImageResources(@NonNull @ArrayRes @Size(min = 2) int[] imageResources){
-    }
-
     public void addImage(String url){
         urls.add(url);
         notifyDataSetChanged();
     }
 
-    public void setOnPagerItemClick(@NonNull OnItemClickListener onPagerItemClickListener){
-        mOnPagerItemClick = onPagerItemClickListener;
-    }
-
-        /*
-        AsyncTask enables proper and easy use of the UI thread. This class
-        allows to perform background operations and publish results on the UI
-        thread without having to manipulate threads and/or handlers.
-     */
-
-    /*
-        final AsyncTask<Params, Progress, Result>
-            execute(Params... params)
-                Executes the task with the specified parameters.
-     */
     private class DownLoadImageTask extends AsyncTask<String,Void,Bitmap> {
         ImageView imageView;
 
@@ -99,19 +70,12 @@ public class ImageViewPagerAdapter extends ViewPagerAdapter {
             this.imageView = imageView;
         }
 
-        /*
-            doInBackground(Params... params)
-                Override this method to perform a computation on a background thread.
-         */
         protected Bitmap doInBackground(String...urls){
             String urlOfImage = urls[0];
             Bitmap logo = null;
             try{
                 InputStream is = new URL(urlOfImage).openStream();
-                /*
-                    decodeStream(InputStream is)
-                        Decode an input stream into a bitmap.
-                 */
+
                 logo = BitmapFactory.decodeStream(is);
             }catch(Exception e){ // Catch the download exception
                 e.printStackTrace();
