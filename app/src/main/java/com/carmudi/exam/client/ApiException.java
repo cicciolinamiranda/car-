4b01fa2b2f3518f4acdb3f0df2440c315a589cf9
Cicciolina Miranda <cicciolina@cicciolina-macbookpro.roam.corp.google.com>
2018-06-19 05:04:22 +0800

    remove http legacy and use retrofit. remove auto gen hidden files

--- app/src/main/java/com/carmudi/exam/client/ApiException.java
+++ app/src/main/java/com/carmudi/exam/client/ApiException.java
@@ -1,9 +1,5 @@
 package com.carmudi.exam.client;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
 /**
  * Created by cicciolina on 5/18/18.
  */
@@ -12,27 +8,7 @@ public class ApiException extends Exception {
     int code = 0;
     String message = null;
 
-    public ApiException() {}
-
     public ApiException(int code, String message) {
-        this.code = code;
-        this.message = message;
-    }
-
-    public ApiException(String s, int code, Map<String, List<String>> stringListMap, String respBody) {
-        this.code = code;
-        this.message = s;
-    }
-
-    public ApiException(IOException e) {
-        this.message = e.getMessage();
-    }
-
-    public ApiException(String s) {
-        this.message = s;
-    }
-
-    public ApiException(String message, IOException e, int code, Map<String, List<String>> stringListMap) {
         this.code = code;
         this.message = message;
     }
